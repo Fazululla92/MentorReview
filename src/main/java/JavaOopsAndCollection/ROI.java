e9cf831e6582e3de26d3a27ea0aedc6f762d0ff9
Fazululla Dudekula <fazululla_dudekula@epam.com>
2026-05-05 15:07:58 +0530

    adding project to git

---
+++ src/main/java/JavaOopsAndCollection/ROI.java
@@ -0,0 +1,57 @@
+package JavaOopsAndCollection;
+
+public class ROI {
+
+//1. High regression effort
+//The system has frequently changing features (widgets, UI formats, configurations), which require repeated testing.
+//
+//2. Repetitive test execution
+//    Core flows like login, page rendering, widget interactions, and sharing features must be tested repeatedly across releases.
+//
+//3. Faster feedback in CI/CD
+//    Automation helps detect defects early and supports continuous integration pipelines.
+//
+//4. Scalability
+//    As the system grows, manual effort increases linearly, while automation scales efficiently.
+//
+//5. Cross-platform testing
+//    Supports multiple environments like mobile and desktop formats.
+//
+//    What can we automate? Why?
+//    Login/logout
+//    Core user flows
+//    Page rendering
+//    Widget combinations
+//
+//    Project duration = 5 years
+//    Manual Testing Cost
+//    20 man-hours/week×5×52=5200 man-hours
+
+//Automation Cost
+//1. Framework setup
+//
+//            = 80 man-hours
+//
+//2. Scenario development
+//
+//            = 40 hours/week for 0.5 year
+//
+//0.5×52=26 weeks
+//40×26=1040 man-hours
+//3. Execution + analysis
+//
+//= 8 hours/week for 5 years
+//
+//8×260=2080 man-hours
+//📌 Total Automation Cost
+//80+1040+2080=3200 man-hours
+//📊 Net Profit
+//5200−3200=2000 man-hours
+//📈 ROI Calculation
+//    ROI=
+//            3200
+//            2000
+//            ​
+//
+//    ROI=0.625=62.5%
+}
